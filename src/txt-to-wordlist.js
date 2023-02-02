e0f10b0cb677dd29e52e0f840bfb2383222038da
Fadlan A <fadlanerman6@gmail.com>
2023-02-02 12:02:38 +0700

    edit wordlist.html

--- src/txt-to-wordlist.js
+++ src/txt-to-wordlist.js
@@ -1,9 +1,15 @@
 fs = require("fs");
-let wordList = fs.readFileSync("../data/translate/id/wordlist-part-1.txt");
-wordList += fs.readFileSync("../data/translate/id/wordlist-part-2.txt");
-wordList += fs.readFileSync("../data/translate/id/wordlist-part-3.txt");
-wordList += fs.readFileSync("../data/translate/id/wordlist-part-4.txt");
+let wordList_id = fs.readFileSync("../data/translate/id/wordlist.txt", "utf-8");
+wordList_id = wordList_id.split(",");
+wordList_id.pop();
+wordList_id.forEach((word, id) => {
+  if (word[0] == " ") {
+    wordList_id[id][0] = "";
+  }
+});
 
-let arr = wordList.split(", ");
-arr.pop();
-console.log(arr[5225]);
+let wordList_en = fs.readFileSync("../data/oxford-5300-wordlist.json");
+wordList_en = JSON.parse(wordList_en);
+
+console.log(wordList_en.length);
+console.log(wordList_id.length);
